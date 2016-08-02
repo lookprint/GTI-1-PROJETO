daab2d344c99d316c0145829037661a9553aeb5e
Patrick Magalhães <pixelzip0@gmail.com>
2016-08-02 02:58:26 -0300

    header menu

--- js/global.js
+++ js/global.js
@@ -3,5 +3,13 @@
  * GTI 01/08/2016
  */
  $(document).ready(function(){
+ 	$(".button-collapse").sideNav();
+
+
+ 	 $('.menu').addClass('animated bounceIn');
+ 	 $('.logomarca').addClass('animated bounceInLeft');
+ 
+
+
 
  });
